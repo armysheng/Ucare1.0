ba754eac66f49b2c48263bbbff30f2cdb74cc54d
armysheng <armysheng@Gmail.com>
2015-01-21 23:23:23 +0800

    change UI and added 4 social login button

--- app/src/main/java/com/armysheng/ucare/Login_Activity.java
+++ app/src/main/java/com/armysheng/ucare/Login_Activity.java
@@ -1,12 +1,11 @@
 package com.armysheng.ucare;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
-public class Login_Activity extends ActionBarActivity {
+
+public class Login_Activity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,25 +13,5 @@ public class Login_Activity extends ActionBarActivity {
         setContentView(R.layout.activity_login);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login_, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
