f7e6efd51f239207f20f48bf426d0e00438ee633
armysheng <armysheng@Gmail.com>
2015-01-23 00:38:07 +0800

    added pulltoRefresh librarys and added homepage to test listview

--- app/src/main/java/com/armysheng/ucare/Login_Activity.java
+++ app/src/main/java/com/armysheng/ucare/Login_Activity.java
@@ -1,8 +1,12 @@
 package com.armysheng.ucare;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.armysheng.ucare.guide.HomePage;
 
 
 public class Login_Activity extends Activity {
@@ -11,7 +15,16 @@ public class Login_Activity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button loginBtn=(Button)findViewById(R.id.login_btn);
+        loginBtn.setOnClickListener(new loginBtnClickListener());
     }
 
-
+    private class loginBtnClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent();
+            intent.setClass(Login_Activity.this, HomePage.class);
+            startActivity(intent);
+        }
+    }
 }
---
+++ app/src/main/java/com/armysheng/ucare/guide/HomePage.java
@@ -0,0 +1,73 @@
+package com.armysheng.ucare.guide;
+
+import android.app.ListActivity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SimpleAdapter;
+
+import com.armysheng.ucare.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class HomePage extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_home_page);
+        SimpleAdapter adapter=new SimpleAdapter(this, getData(), R.layout.list,new String[]{"title","info","img"},
+                new int[]{R.id.title,R.id.info,R.id.img});
+        setListAdapter(adapter);
+    }
+
+    private List<Map<String, Object>> getData() {
+        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("title", "G1");
+        map.put("info", "google 1");
+        map.put("img", R.drawable.btn_qq);
+        list.add(map);
+
+        map = new HashMap<String, Object>();
+        map.put("title", "G2");
+        map.put("info", "google 2");
+        map.put("img", R.drawable.btn_wechat);
+        list.add(map);
+
+        map = new HashMap<String, Object>();
+        map.put("title", "G3");
+        map.put("info", "google 3");
+        map.put("img", R.drawable.btn_weibo);
+        list.add(map);
+
+        return list;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home_page, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
