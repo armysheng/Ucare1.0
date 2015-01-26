338080c32147404bbc85bdead2ae08c6f2858bd2
armysheng <armysheng@Gmail.com>
2015-01-26 23:56:41 +0800

    Add universal-image-loader, and implement simple image load method in pullToRefreshListView

--- app/src/main/java/com/armysheng/ucare/HomePage.java
+++ app/src/main/java/com/armysheng/ucare/HomePage.java
@@ -8,12 +8,18 @@ package com.armysheng.ucare;
  *
  */
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
 import org.json.JSONArray;
 
 import com.armysheng.ucare.R;
@@ -21,6 +27,14 @@ import com.handmark.pulltorefresh.library.PullToRefreshBase;
 import com.handmark.pulltorefresh.library.PullToRefreshListView;
 import com.handmark.pulltorefresh.library.PullToRefreshBase.Mode;
 import com.handmark.pulltorefresh.library.PullToRefreshBase.OnRefreshListener;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
+import com.nostra13.universalimageloader.core.display.RoundedBitmapDisplayer;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
+import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
+import com.nostra13.universalimageloader.utils.L;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -29,6 +43,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.text.format.DateUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -39,14 +54,31 @@ import android.widget.TextView;
 
 public class HomePage extends ListActivity {
 
+    private static final String TEST_FILE_NAME = "Ucare" ;
     private ArrayList<HashMap<String, Object>> listItem = new ArrayList<HashMap<String, Object>>();
     private PullToRefreshListView mPullRefreshListView;
     MyAdapter adapter=null;
+    DisplayImageOptions options;
+    protected ImageLoader imageLoader = ImageLoader.getInstance();
+    private ImageLoadingListener animateFirstListener = new AnimateFirstDisplayListener();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        options = new DisplayImageOptions.Builder()
+                .showImageOnLoading(R.drawable.ic_stub)
+                .showImageForEmptyUri(R.drawable.ic_empty)
+                .showImageOnFail(R.drawable.ic_error)
+                .cacheInMemory(true)
+                .cacheOnDisk(true)
+                .considerExifParams(true)
+                .displayer(new RoundedBitmapDisplayer(20))
+                .build();
+
+        imageLoader.init(ImageLoaderConfiguration.createDefault(this));
+
 
         mPullRefreshListView = (PullToRefreshListView) findViewById(R.id.pull_refresh_list);
 
@@ -75,41 +107,44 @@ public class HomePage extends ListActivity {
         actualListView.setAdapter(adapter);
 
     }
-    private class GetDataTask extends AsyncTask<Void, Void, HashMap<String, Object>> {
+    private class GetDataTask extends AsyncTask<Void, Void, ArrayList<HashMap<String, Object>>> {
 
         //后台处理部分  
         @Override
-        protected HashMap<String, Object> doInBackground(Void... params) {
+        protected ArrayList<HashMap<String, Object>> doInBackground(Void... params) {
             // Simulates a background job.  
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
             }
+            ArrayList<HashMap<String,Object>> newArrayList = new ArrayList<HashMap<String,Object>>();
             HashMap<String, Object> map = new HashMap<String, Object>();
             try {
 
                 map = new HashMap<String, Object>();
                 map.put("name", "林珊");
                 map.put("info", "上传了一张新照片油画");
-                map.put("img","360");
+                map.put("img","assets://7.png");
+                newArrayList.add(map);
+                newArrayList.add(map);
 
             } catch (Exception e) {
-                // TODO: handle exception  
+                // TODO: handle exception  adbe
                 setTitle("map出错了");
                 return null;
             }
 
-            return map;
+            return newArrayList;
         }
 
         //这里是对刷新的响应，可以利用addFirst（）和addLast()函数将新加的内容加到LISTView中  
         //根据AsyncTask的原理，onPostExecute里的result的值就是doInBackground()的返回值  
         @Override
-        protected void onPostExecute(HashMap<String, Object> result) {
+        protected void onPostExecute(ArrayList<HashMap<String, Object>> result) {
             //在头部增加新添内容  
 
             try {
-                listItem.add(result);
+                listItem.addAll(result);
 
                 //通知程序数据集已经改变，如果不做通知，那么将不会刷新mListItems的集合  
                 adapter.notifyDataSetChanged();
@@ -161,12 +196,14 @@ public class HomePage extends ListActivity {
 
         private LayoutInflater mInflater;
 
+//        private ImageLoader imageLoader = new ImageLoader();
+
         public MyAdapter(Context context){
             this.mInflater = LayoutInflater.from(context);
         }
         @Override
         public int getCount() {
-            // TODO Auto-generated method stub  
+            // TODO Auto-generated method stub
             return listItem.size();
         }
 
@@ -200,8 +237,10 @@ public class HomePage extends ListActivity {
 
                 holder = (ViewHolder)convertView.getTag();
             }
-
-            holder.img.setImageBitmap(getHome((String)listItem.get(position).get("img")));
+            String urlString =(String) listItem.get(position).get("img");
+            Log.v("String url:" , urlString);
+            imageLoader.displayImage((String)listItem.get(position).get("img"), holder.img);
+//            holder.img.setImageBitmap(getHome((String)listItem.get(position).get("img")));
             holder.name.setText((String)listItem.get(position).get("name"));
             holder.info.setText((String)listItem.get(position).get("info"));
 
@@ -211,25 +250,25 @@ public class HomePage extends ListActivity {
     }
 
 
-    /**
-     * 根据图片名称获取主页图片 
-     */
-    public Bitmap getHome(String photo){
-        String homeName = photo + ".png";
-        InputStream is=null;
-
-        try {
-            is=getAssets().open("home/"+homeName);
-            Bitmap bitmap = BitmapFactory.decodeStream(is);
-            is.close();
-            return bitmap;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return null;
-
-    }
+//    /**
+//     * 根据图片名称获取主页图片
+//     */
+//    public Bitmap getHome(String photo){
+//        String homeName = photo + ".png";
+//        InputStream is=null;
+//
+//        try {
+//            is=getAssets().open("home/"+homeName);
+//            Bitmap bitmap = BitmapFactory.decodeStream(is);
+//            is.close();
+//            return bitmap;
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//        return null;
+//
+//    }
 
     ////工具类  
     /**
@@ -256,6 +295,45 @@ public class HomePage extends ListActivity {
 
         return readedStr;
     }
+    private void copyTestImageToSdCard(final File testImageOnSdCard) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    InputStream is = getAssets().open(TEST_FILE_NAME);
+                    FileOutputStream fos = new FileOutputStream(testImageOnSdCard);
+                    byte[] buffer = new byte[8192];
+                    int read;
+                    try {
+                        while ((read = is.read(buffer)) != -1) {
+                            fos.write(buffer, 0, read);
+                        }
+                    } finally {
+                        fos.flush();
+                        fos.close();
+                        is.close();
+                    }
+                } catch (IOException e) {
+                    L.w("Can't copy test image onto SD card");
+                }
+            }
+        }).start();
+    }
+    private static class AnimateFirstDisplayListener extends SimpleImageLoadingListener {
 
+        static final List<String> displayedImages = Collections.synchronizedList(new LinkedList<String>());
+
+        @Override
+        public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+            if (loadedImage != null) {
+                ImageView imageView = (ImageView) view;
+                boolean firstDisplay = !displayedImages.contains(imageUri);
+                if (firstDisplay) {
+                    FadeInBitmapDisplayer.animate(imageView, 500);
+                    displayedImages.add(imageUri);
+                }
+            }
+        }
+    }
 
 }  
